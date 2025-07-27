7ab31cab4ff1989b79334282fb010b1470e0ff5f
fpfernandes <fabianapresoto@gmail.com>
2025-07-27 19:52:08 +0100

    Verificacoes de pagina de inventario

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -31,7 +31,16 @@ class TestProdutos():
     def teardown_method(self, method):                     # metodo de finalizacao dos testes
         self.driver.quit()                                 # encerra / destroi o objeto do selenium webdriver da memoria
 
-    def test_selecionar_produto(self):                                              # metodo de teste
-        self.driver.get(self.url)                                                   # abre o navegador
-        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")   # escreve no campo user_name
-        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")   # escreve no campo password  
+    def test_selecionar_produto(self):                                                     # metodo de teste
+        self.driver.get(self.url)                                                          # abre o navegador
+        self.driver.find_element(By.ID, "user-name").send_keys("standard_user")            # escreve no campo user_name
+        self.driver.find_element(By.NAME, "password").send_keys("secret_sauce")            # escreve no campo password  
+        self.driver.find_element(By.CSS_SELECTOR, "input.submit-button.btn_action").click() # click botao login
+
+        # transicao de pagina
+        
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"      # confirma se esta escrito Products no elemento
+        assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"  # confirma se e a mochila
+        # confirma o preco da mochila
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
+
