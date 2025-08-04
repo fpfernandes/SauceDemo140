7e3ffaeeccb734952d325d685a90f77077505212
fpfernandes <fabianapresoto@gmail.com>
2025-08-04 13:20:30 +0100

    Scenario outline solucao sem if

--- features/steps/selecionar_produtos.py
+++ features/steps/selecionar_produtos.py
@@ -13,12 +13,34 @@ def step_impl(context):
     # passo em si
     context.driver.get("https://www.saucedemo.com")     # abrir o navegador no endereco do site alvo
 
-
+# Preencher com usuario e senha
 @when(u'preencho os campos de login com usuario {usuario} e senha {senha}')
 def step_impl(context, usuario, senha):
     context.driver.find_element(By.ID, "user-name").send_keys(usuario)      # preencher o usuario
     context.driver.find_element(By.ID, "password").send_keys(senha)         # preencher a senha
     context.driver.find_element(By.ID, "login-button").click()              # clicar no botao login
+
+# Preencher com usuario em branco e senha
+@when(u'preencho os campos de login com usuario  e senha {senha}')
+def step_impl(context, senha):
+    # nao preenche o usuario
+    context.driver.find_element(By.ID, "password").send_keys(senha)         # preencher a senha
+    context.driver.find_element(By.ID, "login-button").click()              # clicar no botao login
+
+# Preencher com usuario, mas deixar a senha em branco
+@when(u'preencho os campos de login com usuario {usuario} e senha ')
+def step_impl(context, usuario):
+    context.driver.find_element(By.ID, "user-name").send_keys(usuario)      # preencher o usuario
+    # nao preencho a senha
+    context.driver.find_element(By.ID, "login-button").click()              # clicar no botao login
+
+# Clica no botao de login sem ter preenchido o usuario e a senha
+@when(u'preencho os campos de login com usuario  e senha ')
+def step_impl(context):
+    # nao preencho o usuario
+    # nao preencho a senha
+    context.driver.find_element(By.ID, "login-button").click()              # clicar no botao login
+
 
 @then(u'sou direcionado para pagina Home')
 def step_impl(context):
@@ -33,4 +55,14 @@ def step_impl(context):
     # validar a mensagem de error
     assert context.driver.find_element(By.CSS_SELECTOR, "h3").text == "Epic sadface: Username and password do not match any user in this service"
 
+    # teardown / encerramento
+    context.driver.quit()
 
+# Verifica a mensagem para o Scenario Outline
+@then(u'exibe a {mensagem} de erro no login')
+def step_impl(context, mensagem):
+    # validar a mensagem de error
+    assert context.driver.find_element(By.CSS_SELECTOR, "h3").text == mensagem
+
+    # teardown / encerramento
+    context.driver.quit()
